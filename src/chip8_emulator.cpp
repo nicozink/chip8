ea89fe02adeb97c4032f38a548bffdb060e0c011
Nico Zink <nico@nfzink.com>
2015-08-30 13:23:42 +0100

    Development: Added basic project framework.

---
+++ src/chip8_emulator.cpp
@@ -0,0 +1,20 @@
+/*
+Copyright © 2012, Nico Zink
+All rights reserved.
+*/
+
+#include "chip8_emulator.h" 
+
+// Local Includes
+#include "main.h"
+
+// Project Includes
+
+// External Includes
+#include <iostream>
+
+// Runs the main program.
+int run()
+{
+  return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,19 @@
+/*
+Copyright © 2012, Nico Zink
+All rights reserved.
+*/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// Main method which initialises the program.
+// @param argc The number of arguments.
+// @param argv The arguments.
+// @returns The success code.
+int main(int argc, char** argv);
+
+// Runs the main program.
+int run();
+
+#endif
+
